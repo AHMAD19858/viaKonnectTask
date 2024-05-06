555f6e88d0f3f76815ad4ba0c6f0a4cf38142202
Ahmed Essam <aesam@devsolutions.team>
2024-05-06 21:02:51 +0300

    add redirection to home from details page

--- src/components/Global.tsx
+++ src/components/Global.tsx
@@ -4,11 +4,14 @@ import Image from 'next/image';
 import x from '../../public/socials/x.png'
 import facebook from '../../public/socials/facebook.png'
 import instagram from '../../public/socials/instagram.png'
+import Link from 'next/link';
 function global() {
 
   return (
    <div className=' flex justify-center '>
+      <Link href='/'>
         <p className=' font-black text-xl'>viaKonnect.<span className=' text-sm font-semibold'>news</span></p>
+      </Link>
        <div className=' absolute right-0 flex gap-2 items-center mx-8 pt-2'>
        <Image
           className=' cursor-pointer '
